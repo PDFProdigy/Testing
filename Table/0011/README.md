382c7aedbe9d5f8777021de22b224fea475d309b
pdfprodigy <pdfprodigy@gmail.com>
2014-04-20 09:44:24 +0200

    add new PDF table extraction test: test_0011

---
+++ Table/0011/README.md
@@ -0,0 +1,25 @@
+test_0011
+=========
+
+"test_0011" is for testing table extraction in [PDFProdigy](http://www.pdfprodigy.com) with a very simple table with all the borders, no header and no spanning.  
+However, the lines of each cell border do not connect to each other (as in [test_0009](../test_0009/) and [test_0010](../test_0010/) but with a smaller gap size of 0.8), and there are three lines of text (aligned to the right) in each cell (separated by a newline).
+
+![test_0011 screenshot](test_0011.png)
+
+This folder contains 6 PDF files with this _testing table_, one file for each PDF version ([from version 1.2 to 1.7](http://en.wikipedia.org/wiki/Portable_Document_Format)).
+
+Those PDF files were created by a Java program (_Cf._ test_XXXX.java in this folder) developped thanks to the ([IText Java library](http://itextpdf.com/))
+
+File list of this folder: 
+
+   - **README.md**: this file
+   - **test_XXXX_v1.2.pdf**: the PDF file in version 1.2
+   - **test_XXXX_v1.3.pdf**: the PDF file in version 1.3
+   - **test_XXXX_v1.4.pdf**: the PDF file in version 1.4
+   - **test_XXXX_v1.5.pdf**: the PDF file in version 1.5
+   - **test_XXXX_v1.6.pdf**: the PDF file in version 1.6
+   - **test_XXXX_v1.7.pdf**: the PDF file in version 1.7
+   - **test_XXXX.png**: a screenshot of the table in PDF format
+   - **test_XXXX.java**: the source of the Java program which created all the PDF files
+   - **compile.bat**: the DOS command line to compile the Java program
+   - **run.bat**: the DOS command line to run the Java program
---
+++ Table/0011/test_0011.java
@@ -0,0 +1,138 @@
+/**
+ * This program creates a PDF file with a table in different PDF version (1.2 to 1.7).
+ * The purpose of this PDF file is to test and control the efficacity of table extraction from PDF documents.
+ *
+ * This code was developped thanks to the Java library IText (http://itextpdf.com/)
+ * The jar file can be retrieved from the maven repository : http://mvnrepository.com/artifact/com.itextpdf/itextpdf
+ * The IText API documentation is available here : http://api.itextpdf.com/itext/index.html?overview-summary.html
+ *
+ * To compile : javac -cp ..\..\Lib\itextpdf-5.5.0.jar   test_xxxx.java
+ * To run     : java  -cp ..\..\Lib\itextpdf-5.5.0.jar;. test_xxxx
+ */
+
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.Section;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfContentByte;
+import com.itextpdf.text.BadElementException;
+import com.itextpdf.text.pdf.BaseFont;
+
+public class test_0011
+{
+   public static final String PDF_FILENAME = "test_0011";
+    
+
+   private static void
+   writeTable(PdfWriter __writer)
+      throws BadElementException, DocumentException, IOException
+      {
+         PdfContentByte canvas = __writer.getDirectContent();
+         canvas.saveState();
+         canvas.setLineWidth(0.1f);
+
+         float origin_x    = 50f;
+         float origin_y    = 600f;
+         float cell_width  = 80f;
+         float cell_height = 60f;
+
+         // the gap size between each cell borders.
+         float separator = 0.8f;
+         for (int i=0; i <= 5; i++)
+            {
+               for (int j=0; j <= 3; j++)
+                  {
+                     if (i < 5)
+                        {
+                           // Horizontal line
+                           canvas.moveTo( origin_x+separator+    i*cell_width, origin_y+j*cell_height);
+                           canvas.lineTo( origin_x-separator+(i+1)*cell_width, origin_y+j*cell_height);
+                           canvas.stroke();                           
+                        }
+
+                     if (j < 3)
+                        {
+                           // Vertical Line
+                           canvas.moveTo( origin_x+i*cell_width, origin_y+separator+    j*cell_height);
+                           canvas.lineTo( origin_x+i*cell_width, origin_y-separator+(j+1)*cell_height);
+                           canvas.stroke();
+                        }
+                  }
+            }
+
+         BaseFont bf = BaseFont.createFont();
+
+         String text_line2[] = { "first", "", "", "", "fifth"};
+         String text_line3[] = { "nothing", "", "", "", ""};
+
+         canvas.beginText();
+         canvas.setFontAndSize(bf, 10);
+         for (int i=0; i<5; i++)
+            {
+               for (int j=0; j<3; j++)
+                  {
+
+                     canvas.showTextAligned(Element.ALIGN_RIGHT, "Cell = ("+(3-j)+","+(i+1)+")",  origin_x+cell_width*i+cell_width/1.1f, origin_y+cell_height*j+(cell_height/4)*3, 0);
+
+                     if (text_line2[i].length() > 0)
+                        {
+                           canvas.showTextAligned(Element.ALIGN_RIGHT, text_line2[i],             origin_x+cell_width*i+cell_width/1.1f, origin_y+cell_height*j+(cell_height/4)*2, 0);
+                        }                     
+
+                     if (text_line3[i].length() > 0)
+                        {
+                           canvas.showTextAligned(Element.ALIGN_RIGHT, text_line3[i],             origin_x+cell_width*i+cell_width/1.1f, origin_y+cell_height*j+(cell_height/4)*1, 0);
+                        }                     
+                  }
+            }
+         canvas.endText();
+
+         canvas.restoreState();
+      }
+
+
+   public static void
+   createPDFFile(char __pdf_version)
+      throws DocumentException, IOException 
+      {
+         Document  document = new Document();
+         PdfWriter writer   = PdfWriter.getInstance(document, new FileOutputStream(PDF_FILENAME+"_v1."+__pdf_version+".pdf"));
+         writer.setPdfVersion(__pdf_version);
+
+         document.open();
+        
+         // Set the metadata
+         document.addTitle(PDF_FILENAME);
+         document.addSubject("Testing table extraction from PDF file");
+         document.addKeywords("PDFProdigy, testing, table, extract");
+         document.addAuthor("PDFProdigy.com");
+         document.addCreator("PDFProdigy.com");
+
+         // Add the table
+         writeTable(writer);
+
+         document.close();
+      }
+
+
+   public static void 
+   main(String[] __args)
+      throws DocumentException, IOException 
+      {
+         createPDFFile(PdfWriter.VERSION_1_2);
+         createPDFFile(PdfWriter.VERSION_1_3);
+         createPDFFile(PdfWriter.VERSION_1_4);
+         createPDFFile(PdfWriter.VERSION_1_5);
+         createPDFFile(PdfWriter.VERSION_1_6);
+         createPDFFile(PdfWriter.VERSION_1_7);
+      }
+}
